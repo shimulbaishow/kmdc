6a8bc85de6e9a907d3409e74150285db4b07ca5d
shimulbaishow <shimulbaishow@gmail.com>
2025-10-29 10:48:04 +0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,54 @@
+ 
+       // document.write("Hello World");
+       // alert("Hello World");
+        //console.log("Hello World");
+    
+  //Input Taking 
+     
+        // var name=prompt("Input Your Name:");
+
+        // document.write("Hello "+name);
+
+    //     var a=10;
+
+    //    //console.log(a **2);
+
+    //    let num=50;
+    //    num=54;
+      
+      //  //console.log(num);
+      //  var x=10;
+      //  var y='10';
+      //  console.log(x===y);
+      //  console.log(x!==y);
+      //  console.log(x!=y);
+      //  var z=50;
+      //  let strr=z<x? 'shimul': 'baishow';
+      //  console.log(strr);
+
+      //  //Logical Operator
+      //  var x=true; var y=false;
+      //  var z=x && y;
+      //  console.log(z);
+
+      //  var z=x || y;
+      //  console.log(z);
+
+      //  var z=x ~ y;
+      //  console.log(z);
+
+
+      //Array 
+  var city=["Dhaka","Rangpur","Cumilla","Barishal"];
+  //Adding element from first
+  var remove_city=city.shift()
+  console.log(city);
+  //Removing element from first
+  var add_ele=city.unshift("Khulna");
+  console.log(city);
+
+  //Adding element form end
+  var add_el=city.push("Syllet");
+  console.log(city);
+  var re_el=city.pop();
+  console.log(city);
